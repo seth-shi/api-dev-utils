200a5518ca7adbbe2ab6cf61fb54944d7e1b4404
seth-shi <shi.guopeng@csdigit.com>
2020-06-10 18:49:12 +0800

    init cache middleware response

---
+++ config/utils.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    
+    'api_cache' => [
+        'enable' => env('API_CACHE'),
+        'global_ttl' => 60 * 60,
+        'routes' => [
+        
+        ]
+    ]
+];
---
+++ src/Middleware/CacheResponseMiddleware.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace SethShi\ApiDevUtils\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Config;
+
+class CacheResponseMiddleware
+{
+    /**
+     * @param $request
+     * @param Closure $next
+     * @param null $guard
+     * @return \Illuminate\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector|mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $cacheConfig = Config::get('utils.api_cache', []);
+    
+        if (! $cacheConfig['enable']) {
+            return $next($request);
+        }
+    
+        // only cache http get request
+        if (! $request->isMethod('GET')) {
+            return $next($request);
+        }
+        
+        
+        $uri = $request->route()->getUri();
+        if (! isset($cacheConfig['routes'][$uri])) {
+            
+            return $next($request);
+        }
+        
+        $routeConfig = $cacheConfig['routes'][$uri];
+        $parameters = $this->assemble($routeConfig[0] ?? [], $request->all());
+        $headers = $this->assemble($routeConfig[1] ?? [], $request->headers->all());
+        $ttl = $routeConfig[3] ?? $cacheConfig['global_ttl'];
+        
+        $cacheKey = $uri . ':' . http_build_query(array_merge($parameters, $headers));
+        // cache options
+        $cacheStore = Cache::tags($uri);
+        
+        $cacheResponse = $cacheStore->get($cacheKey);
+        if (is_null($cacheResponse)) {
+            
+            $cacheResponse = $next($request);
+            $cacheStore->put($cacheKey, $cacheResponse, $ttl);
+        }
+        
+        return $cacheResponse;
+    }
+    
+    protected function assemble(array $fields, array $data)
+    {
+        if (empty($fields) || empty($data)) {
+            return [];
+        }
+    
+        return Arr::sortRecursive(Arr::only($data, $fields));
+    }
+}
